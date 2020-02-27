1771305b496d1dee3dd7f0593d9589a654a64318
Justin Ethier <justin.ethier@gmail.com>
2020-02-27 16:26:26 -0500

    Initial file

---
+++ package.scm
@@ -0,0 +1,15 @@
+(package
+ (name           srfi-152)
+ (version        0.1)
+ (license        "MIT")
+ (authors        "John Cowan")
+ (maintainers    "Justin Ethier <justin.ethier at gmail dot com>")
+ (description    "String Library (reduced)")
+ (tags           "srfi")
+ (docs           "https://github.com/cyclone-scheme/cyclone-winds/wiki/srfi-152")
+ (test           "cyclone-test.scm")
+
+ (library
+     (name (srfi 152))
+   (description "String Library")))
+
